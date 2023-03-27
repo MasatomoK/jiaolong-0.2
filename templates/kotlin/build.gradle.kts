2a55ff5bcb783cde922a6d3e6fcbbd36d2775b29
Masatomo <masatomo.koba@gmail.com>
2023-03-27 23:17:31 +0900

    refactoring: kotlin template

--- templates/kotlin/build.gradle.kts
+++ templates/kotlin/build.gradle.kts
@@ -2,4 +2,8 @@ plugins {
     setup.kotlin
 }
 
-dependencies {}
+dependencies {
+    implementation(lib.bundles.common.kotlin.implementation)
+
+    testImplementation(lib.bundles.common.kotlin.testImplementation)
+}
